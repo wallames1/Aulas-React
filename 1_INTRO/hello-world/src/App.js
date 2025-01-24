c23021941031463595abd2b1b727944f88de3f24
wallames dos anjos <110430324+wallames1@users.noreply.github.com>
2025-01-24 18:52:19 -0300

    desafio 1

--- 1_INTRO/hello-world/src/App.js
+++ 1_INTRO/hello-world/src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1> HELLO-WORLD</h1>
+      <p className="p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus ea, cupiditate mollitia in, qui quaerat aliquid quas sit, eveniet cumque repellendus velit dolor quidem commodi exercitationem! Doloremque unde harum facilis.</p>
     </div>
   );
 }
