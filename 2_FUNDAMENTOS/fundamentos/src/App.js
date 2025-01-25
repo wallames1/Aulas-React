b7b94e76826354537f941b10ac222a1e44f03ff5
wallames dos anjos <110430324+wallames1@users.noreply.github.com>
2025-01-24 21:15:37 -0300

    inicio do segundo modulo

---
+++ 2_FUNDAMENTOS/fundamentos/src/App.js
@@ -0,0 +1,13 @@
+//components
+import FirstComponent from './components/FirstComponent';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <FirstComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
@@ -0,0 +1,9 @@
+const FirstComponent = () => {
+    return (
+        <div>
+            <h1>MEU PRIMEIRO COMPONENT</h1>
+        </div>
+    );
+};
+
+export default FirstComponent;
