47025163d32a7a2ab88f3d6d5ee0e0f20d1a8b18
wallames dos anjos <110430324+wallames1@users.noreply.github.com>
2025-01-29 21:18:29 -0300

    if ternario

--- 3_AVANCANDO_EM_REACT/avancando/src/components/ConditionalRender.js
+++ 3_AVANCANDO_EM_REACT/avancando/src/components/ConditionalRender.js
@@ -2,12 +2,23 @@ import { useState } from "react"
 
 const ConditionalRender = () => {
     const [x] = useState(true);
+
+    const [name,setName] = useState("wallames")
   return (
     <div>
-        <h1>isso será exibido ?</h1>
+        <h1>ISSO SERÀ EXIBIDO ?</h1>
         {
             x && <p>Se x for true, Sim</p>
         }
+        <h1>IF TERNARIO</h1>
+        {name === "wallames" ? (
+        <div>
+          <p>o nome é wallames</p>
+        </div>
+      ) : (<div>
+        <p>o nome não é wallames</p>
+      </div>)}
+      <button onClick={()=> setName("joao")}>TROCAR</button>
     </div>
   )
 }
