ab8a99d696bdbf753776abd0479386f7a39fc144
wallames dos anjos <110430324+wallames1@users.noreply.github.com>
2025-02-01 20:07:38 -0300

    css em component

--- 4_CSS_REACT/css/src/App.js
+++ 4_CSS_REACT/css/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import MyComponent from "./components/MyComponent";
 
 function App() {
   return (
     <div className="App">
-      <h1>CSS GLOBAL</h1>
+      {/**css de global */}
+      <h1>REACT COM CSS</h1>
+      {/**css de componente */}
+      <MyComponent/>
+      <p>este paragrafo é do app.js</p>
+      <p className="test" >este paragrafo é do app.js</p>
     </div>
+
   );
 }
 
---
+++ 4_CSS_REACT/css/src/components/MyComponent.js
@@ -0,0 +1,12 @@
+import "./MyComponent.css"
+
+const MyComponent = () => {
+  return (
+    <div className="App">
+      <h1>CSS DE COMPONENT</h1>
+      <p>Este é o paragrafo do componente</p>
+    </div>
+  );
+};
+
+export default MyComponent;
