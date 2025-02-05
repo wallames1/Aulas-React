b5e8d814dc162041ca604540c20a8a76b22509b9
wallames dos anjos <110430324+wallames1@users.noreply.github.com>
2025-02-05 10:42:53 -0300

    set email inline
    usar apenas quando preciisar somente do valor do input

--- 5_FORM_EM_REACT/form/src/components/MyForm.js
+++ 5_FORM_EM_REACT/form/src/components/MyForm.js
@@ -1,14 +1,29 @@
 import './MyForm.css'
+import {useState} from 'react'
 
 function MyForm() {
+    //3 - gerenciamento de dados
+    const [name, setName] = useState()
+    const [email, setEmail] = useState()
+
+    const handleName = (e) => {
+        setName(e.target.value)
+    }
+
+
   return (
     <div>
         {/*1-criação de form*/}
         <form>
             <div>
                 <label htmlFor="name">Nome:</label>
-                <input type="text" name="name" placeholder="digite o seu nome"/>
+                <input type="text" name="name" placeholder="digite o seu nome" onChange={handleName}/>
             </div>
+            {/*2-label envolvendo input*/}
+            <label>
+                <span>E-mail:</span>
+                <input type="email" name="email" placeholder="Digite seu E-mail" onChange={(e)=>setEmail(e.target.value)}/>
+            </label>
             <input type="submit" value="Enviar"/>
         </form>
     </div>
