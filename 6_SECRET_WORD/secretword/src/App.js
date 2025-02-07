944e4fd88b7a0f7365f61685b0c9de699fe0750a
wallames dos anjos <110430324+wallames1@users.noreply.github.com>
2025-02-07 19:06:23 -0300

    adicionando objetos

--- 6_SECRET_WORD/secretword/src/App.js
+++ 6_SECRET_WORD/secretword/src/App.js
@@ -1,11 +1,34 @@
 import logo from './logo.svg';
+//css
 import './App.css';
+//components
 import StartScreen from './components/StartScreen';
+//React
+import { use, useCallback,useEffect,useState } from 'react';
+//data
+import {wordsList} from "./data/word"
+import Game from './components/Game';
+import GameOver from './components/GameOver';
 
+const stages = [
+  {id:1, name:"start"},
+  {id:2, name:"game"},
+  {id:3, name:"end"},
+
+  
+]
 function App() {
+  const[gameStage, setGameStage] = useState(stages[0].name)
+  const [words] = useState(wordsList)
+
+  console.log(words)
+  
   return (
     <div className="App">
-      <StartScreen/>
+      {gameStage === "start" && <StartScreen/>}
+      {gameStage === "game" && <Game/>}
+      {gameStage === "end" && <GameOver/>}
+
     </div>
   );
 }
