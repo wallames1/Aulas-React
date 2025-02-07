9ce45612fa07b4776ae0f7c209269635a0d8bd42
wallames dos anjos <110430324+wallames1@users.noreply.github.com>
2025-02-07 18:37:43 -0300

    tela que mandei pro natan

--- 6_SECRET_WORD/secretword/src/components/StartScreen.js
+++ 6_SECRET_WORD/secretword/src/components/StartScreen.js
@@ -1,11 +1,23 @@
+import { useState } from "react"
 import "./StartScreen.css"
 
 const StartScreen = () => {
+  
+  const natan = "NATHAN VIADO"
+  
+  // Estado para controlar a visibilidade do conteúdo
+  const [showContent, setShowContent] = useState(false)
+
+  // Função que altera o estado para mostrar o conteúdo
+  const handleStartClick = () => {
+    setShowContent(true)
+  }
   return (
     <div className="start">
       <h1>Secret Word</h1> 
       <p>Clique no botão abaixo para começar a jogar</p>
-      <button>Começar a jogar</button>
+      <button onClick={handleStartClick}>Começar a jogar</button>
+      {showContent && <p>{natan}</p>}
     </div>
   )
 }
