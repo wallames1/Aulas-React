5cdf0912e9320c9e5b83daf4ebf431212361fa51
wallames dos anjos <110430324+wallames1@users.noreply.github.com>
2025-02-08 18:23:49 -0300

    criação das pag

--- 6_SECRET_WORD/secretword/src/App.js
+++ 6_SECRET_WORD/secretword/src/App.js
@@ -20,14 +20,24 @@ const stages = [
 function App() {
   const[gameStage, setGameStage] = useState(stages[0].name)
   const [words] = useState(wordsList)
+// starts the secret game
+  const startGame = () => {
+    setGameStage(stages[0].name)
+  }
+  // process the letter input
+  const verifyLetter = () =>{
+    setGameStage(stages[2].name)
+  }
 
-  console.log(words)
-  
+  // restarts the game
+  const retry = () => {
+    setGameStage(stages[0].name)
+  }
   return (
     <div className="App">
-      {gameStage === "start" && <StartScreen/>}
-      {gameStage === "game" && <Game/>}
-      {gameStage === "end" && <GameOver/>}
+      {gameStage === "start" && <StartScreen startGame={startGame}/>}
+      {gameStage === "game" && <Game verifyLetter={verifyLetter}/>}
+      {gameStage === "end" && <GameOver retry={retry}/>}
 
     </div>
   );
--- 6_SECRET_WORD/secretword/src/components/StartScreen.js
+++ 6_SECRET_WORD/secretword/src/components/StartScreen.js
@@ -1,23 +1,12 @@
 import { useState } from "react"
 import "./StartScreen.css"
 
-const StartScreen = () => {
-  
-  const natan = "NATHAN VIADO"
-  
-  // Estado para controlar a visibilidade do conteúdo
-  const [showContent, setShowContent] = useState(false)
-
-  // Função que altera o estado para mostrar o conteúdo
-  const handleStartClick = () => {
-    setShowContent(true)
-  }
+const StartScreen = ({startGame}) => {
   return (
     <div className="start">
       <h1>Secret Word</h1> 
       <p>Clique no botão abaixo para começar a jogar</p>
-      <button onClick={handleStartClick}>Começar a jogar</button>
-      {showContent && <p>{natan}</p>}
+      <button onClick={startGame}>Começar a jogar</button>
     </div>
   )
 }
