16c337c00fe6ef4e7942008632c1e31746679d60
wallames dos anjos <110430324+wallames1@users.noreply.github.com>
2025-02-19 10:55:07 -0300

    metodo DELETE

--- 7_REQ_HTTP/httpreact/src/App.js
+++ 7_REQ_HTTP/httpreact/src/App.js
@@ -12,7 +12,7 @@ function App() {
   const [products, setProducts] = useState([])
 
   // 4 custom
-  const {data: items, httpConfig, loading} = useFetch(url)
+  const {data: items, httpConfig, loading, error} = useFetch(url)
 
  /*  useEffect(() => {
     async function fetchData() {
@@ -61,17 +61,23 @@ function App() {
     setPrice ("")
 
   }
+  //8 -desafio 6
+  const handleRemove = (id) => {
+    httpConfig(id, "DELETE")
+  }
+  
   return (
     <div className="App">
       <h1>Lista de produtos</h1>
       {/*6 - loading*/}
       {loading && <p>CARREGANDO...</p>}
-      
-      {!loading && (
+      {error && <p>{error}</p>}
+      {!error && (
         <ul>
         {items && items.map((product) =>(
           <li key={product.id}>
             {product.name} - R$ {product.price}
+            <button onClick={() => handleRemove(product.id)}> EXCLUIR</button>
           </li>
         ))}
       </ul>
--- 7_REQ_HTTP/httpreact/src/hooks/useFetch.js
+++ 7_REQ_HTTP/httpreact/src/hooks/useFetch.js
@@ -13,6 +13,11 @@ export const useFetch = (url) => {
     //6 - loading
     const [loading, setLoading] = useState(false)
 
+    //7- tratando erros
+    const [error, setError] = useState(null)
+    //8- desafio 6
+    const [itemId, setItemId] = useState(null)
+
     const httpConfig = (data, method) => {
         if(method === "POST") {
             setConfig({
@@ -24,6 +29,15 @@ export const useFetch = (url) => {
             })
 
             setMethod(method)
+        }else if (method === "DELETE") {
+                setConfig({
+                    method,
+                    headers: {
+                    "Content-type": "application/json"
+                    },
+                })
+                setMethod(method)
+                setItemId(data)
         }
     }
 
@@ -33,11 +47,18 @@ export const useFetch = (url) => {
             //6 - loading
             setLoading(true)
 
-            const res = await fetch(url)
+            try {
 
-            const json = await res.json()
+                const res = await fetch(url)
+                const json = await res.json()
 
-            setData(json)
+                setData(json)
+
+            } catch (error) {
+
+                console.log(error.message)
+                setError("houve algum erro ao carregar")
+            }
             
             setLoading(false)
         }
@@ -48,17 +69,26 @@ export const useFetch = (url) => {
     //5 - refatorando post
     useEffect(() => {
         const httpRequest = async () => {
+
+            let json 
+
             if(method === "POST"){
                 let fetchOptions = [url, config]
     
                 const res = await fetch(...fetchOptions)
-                const json = await res.json()
+                 json = await res.json()
     
                 setCallFetch(json)
+            }else if (method === "DELETE"){
+                const deleteUrl = `${url}/${itemId}`
+                const res = await fetch(deleteUrl, config)
+                 json = await res.json()
             }
+            setCallFetch(json)
         }
+        
 
         httpRequest()
     },[config, method, url])
-    return {data, httpConfig, loading}
+    return {data, httpConfig, loading, error}
 }
